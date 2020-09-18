3507b971c3aee256fea31857b948ae8cb83d3da5
[suraj] <[suraj1996xyz@gmail.com]>
2020-09-18 16:18:46 +0530

    Updates

---
+++ precache-manifest.4c81daac752dac46e914f5b07d9ad7a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a8207be2acfcd4505b1fa461db0ce2",
+    "url": "/coronatracker/index.html"
+  },
+  {
+    "revision": "367f2aa9595dee9dfae0",
+    "url": "/coronatracker/static/css/main.d03ac455.chunk.css"
+  },
+  {
+    "revision": "3191fb6b7b4ea20e02bf",
+    "url": "/coronatracker/static/js/2.b7046017.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/coronatracker/static/js/2.b7046017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367f2aa9595dee9dfae0",
+    "url": "/coronatracker/static/js/main.cc6dc54e.chunk.js"
+  },
+  {
+    "revision": "f4b93a2bbc8f3d5e3220",
+    "url": "/coronatracker/static/js/runtime-main.38a8dd27.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronatracker/static/media/image.d7265326.png"
+  }
+]);
